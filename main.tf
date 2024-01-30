98dab1de15f12c2e6b61e67972bb9a33e240c75b
srujanreddybaddam <srujanreddy214.com>
2024-01-30 18:31:08 -0500

    text added

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2_instance" {
     
     ami = "ami-09694bfab577e90b0"
     instance_type = "t2.micro"
-    //availability_zone = "us-east-2a"
+    availability_zone = "us-east-2a"
     associate_public_ip_address = true
     tags = {
       "Name"="example"
