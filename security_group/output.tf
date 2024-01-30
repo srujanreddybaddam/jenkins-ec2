f7bdb9a747aa8bbe74b4f90e635dd9886f873c43
srujan reddy <144186490+srujanreddybaddam@users.noreply.github.com>
2024-01-30 12:49:11 -0500

    Delete security_group directory

--- security_group/output.tf
+++
@@ -1,3 +0,0 @@
-output "aws_security_group" {
-  value = aws_security_group.securitygroup.id
-}
--- security_group/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "sg" {
-  type = string
-}
-
-variable "some_custom_vpc" {
-  default = "vpc-03e72860710d8cf59"
-}
