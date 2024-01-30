8f809f416ff6807001f8a3fec6098fe95e6595f2
srujan reddy <144186490+srujanreddybaddam@users.noreply.github.com>
2024-01-30 12:49:22 -0500

    Delete subnet directory

--- subnet/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "some_public_subnet" {
-  vpc_id            = var.some_custom_vpc
-  cidr_block        = var.cidr_block
-  availability_zone = var.availability_zone
-
-  tags = {
-    Name = "Public Subnet"
-  }
-}
--- subnet/output.tf
+++
@@ -1,4 +0,0 @@
-output "subnet_id" {
-  description = "subnet id"
-  value       = aws_subnet.some_public_subnet.id
-}
