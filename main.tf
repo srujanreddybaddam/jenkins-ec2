78cdc9514a955edd6f5bbb137caf074a37ad5dec
srujan reddy <144186490+srujanreddybaddam@users.noreply.github.com>
2025-03-21 16:25:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform { 
+ required_providers {
+ aws = {
+ source = "hashicorp/aws"
+ version = "~> 4.16"
+ }
+ }
+ required_version = ">=1.2.0" # which means any version equal & above 1.1 like 1.2, 1.3 etc
+}
+
 provider "aws" {
   region     = "us-east-2"
 }
@@ -5,10 +15,10 @@ provider "aws" {
 resource "aws_instance" "ec2_instance" {
     
     ami = "ami-0d0f28110d16ee7d6"
-    instance_type = "t2.micro"
+    instance_type = "t3.medium"
     availability_zone = "us-east-2a"
     associate_public_ip_address = true
     tags = {
-      "Name"="example"
+      "Name"="demo-ec2"
     }
 } 
